d55657664b94a990dca18784627e42353e83422e
Erik Henning Thiede <ehthiede@gmail.com>
2017-11-02 19:51:14 -0500

    DEV : tweaked package structure

--- setup.py
+++ setup.py
@@ -25,14 +25,14 @@ def read(*names, **kwargs):
 setup(
     name='pyDiffMap',
     version='0.1.0',
-    license='MIT license',
+    license='GPL license',
     description='Library for constructing variable bandwidth diffusion maps',
     long_description='%s\n%s' % (
         re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
         re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
     ),
-    author='Zofia Trstanova',
-    author_email='zofia.trstanova@ed.ac.uk',
+    author='Zofia Trstanova, Erik Henning Thiede',
+    author_email='zofia.trstanova [at symbol] ed.ac.uk, ehthiede [at symbol] gmail.com',
     url='https://github.com/ZofiaTr/python-pydiffmap',
     packages=find_packages('src'),
     package_dir={'': 'src'},
@@ -41,8 +41,9 @@ setup(
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Scientific/Engineering'
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Operating System :: Unix',
         'Operating System :: POSIX',
@@ -50,23 +51,16 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        # uncomment if you test on these interpreters:
-        # 'Programming Language :: Python :: Implementation :: IronPython',
-        # 'Programming Language :: Python :: Implementation :: Jython',
-        # 'Programming Language :: Python :: Implementation :: Stackless',
-        'Topic :: Utilities',
+        'Topic :: Scientific/Engineering',
     ],
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     install_requires=[
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        'numpy', 'scipy', 'sklearn'
     ],
     extras_require={
         # eg:
