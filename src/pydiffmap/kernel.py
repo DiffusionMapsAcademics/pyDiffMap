79026fe9d6fad05048c62228d61261320689e915
Erik Henning Thiede <ehthiede@gmail.com>
2017-11-08 09:49:33 -0600

    ENH : Documentation and Code enhancement in kernel
    - Changed "distance" to "metric" in the kernel class and added ability to
    pass parameters for scipy pairwise metric compatability
    - Added docstrings

--- src/pydiffmap/kernel.py
+++ src/pydiffmap/kernel.py
@@ -7,37 +7,73 @@
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
-class Kernel():
+class Kernel(object):
+    """
+    Class abstracting the evaluation of kernel functions on the dataset.
+    """
 
-    def __init__(self, type='gaussian', distance = 'euclidean', epsilon = 1.0, k=64):
+    def __init__(self, type='gaussian', epsilon = 1.0, k=64, metric='euclidean', metric_params=None):
+        """
+        Initializes the kernel object.
 
+        Parameters
+        ----------
+        type : string, optional
+            Type of kernel to construct. Currently the only option is 'gaussian', but more will be implemented.
+        epsilon : scalar, optional
+            Value of the length-scale parameter. Default is 1.
+        k : int, optional
+            Number of nearest neighbors over which to construct the kernel.  Default is 64.
+        metric : string, optional
+            Distance metric to use in constructing the kernel.  This can be selected from any of the scipy.spatial.distance metrics, or a callable function returning the distance.
+        metric_params : dict or None, optional
+            Optional parameters required for the metric.
 
+        """
         self.type = type
         self.epsilon = epsilon
-        self.distance = distance
+        self.metric = metric
         self.k = k
 
     def fit(self, X):
+        """
+        Fits the kernel to the data X, constructing the nearest neighbor tree.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_query, n_features)
+            Data upon which to fit the nearest neighbor tree.
+
+        Returns
+        -------
+        self : the object itself.
+        """
         self.k0 = min(self.k, np.shape(X)[0])
         self.data = X
-        self.neigh = NearestNeighbors(metric=self.distance).fit(X)
+        self.neigh = NearestNeighbors(metric=self.metric).fit(X)
         return self
 
     def compute(self, Y=None):
         """
-        compute sparse kernel matrix
-        input:  X = (n,d) numpy array of X data points,
-                Y = (m,d) numpy array of Y data points,
-                rows correspond to different observations,
-                columns to different variables
-        output: sparse n x m kernel matrix k(X,Y).
+        Computes the sparse kernel matrix.
+
+        Parameters
+        ----------
+        Y : array-like, shape (n_query, n_features), optional.
+            Data against which to calculate the kernel values.  If not provided, calculates against the data provided in the fit.
+
+        Returns
+        -------
+        K : array-like, shape (n_query_X, n_query_Y)
+            Values of the kernel matrix. 
+        
         """
         # perform k nearest neighbour search on X and Y and construct sparse matrix
-        A = self.neigh.kneighbors_graph(Y,n_neighbors=self.k0, mode='distance')
+        K = self.neigh.kneighbors_graph(Y,n_neighbors=self.k0, mode='distance')
         # retrieve all nonzero elements and apply kernel function to it
-        v = A.data
+        v = K.data
         if (self.type=='gaussian'):
-            A.data = np.exp(-v**2/self.epsilon)
+            K.data = np.exp(-v**2/self.epsilon)
         else:
             raise("Error: Kernel type not understood.")
-        return A
+        return K
