5be8f55f8dda8f397b1715f5d44a614dd45b5641
Erik Henning Thiede <ehthiede@gmail.com>
2017-11-08 23:43:33 -0600

    TST : Started test case for neighborlists in kernel matrix

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -5,7 +5,7 @@ from pydiffmap import kernel
 from scipy.spatial.distance import cdist
 
 x_values = np.vstack((np.linspace(-1, 1, 11), np.arange(11))).T  # set of X vals
-y_values_set = [None, x_values, np.arange(22).reshape(11, 2)]  # all sets of Y's
+y_values_set = [None, x_values, np.arange(6).reshape(-1,2), np.arange(44).reshape(-1, 2)]  # all sets of Y's
 epsilons = [10., 1., 0.1]  # Possible epsilons
 
 
@@ -13,8 +13,11 @@ class TestKernel(object):
     # These decorators run the test against all possible y, epsilon values.
     @pytest.mark.parametrize('y_values', y_values_set)
     @pytest.mark.parametrize('epsilon', epsilons)
-    def test_variable_y_inputs(self, y_values, epsilon):
-        # Setup Reference values
+    def test_matrix_output(self, y_values, epsilon):
+        """
+        Test that we are returning the correct kernel values.
+        """
+        # Setup true values to test again.
         if y_values is None:
             y_values_ref = x_values
         else:
@@ -22,13 +25,33 @@ class TestKernel(object):
         pw_distance = cdist(y_values_ref, x_values, metric='sqeuclidean')
         true_values = np.exp(-1.*pw_distance/epsilon)
 
+        # Construct the kernel and fit to data.
         mykernel = kernel.Kernel(type='gaussian', metric='euclidean',
                                  epsilon=epsilon, k=len(x_values))
         mykernel.fit(x_values)
         K_matrix = mykernel.compute(y_values).toarray()
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print(K_matrix)
-        print(true_values)
+
+        # Check that error values are beneath tolerance.
         error_values = (K_matrix-true_values).ravel()
-#        print(error_values)
-        assert(np.linalg.norm(error_values) < 1E-6)
+        total_error = np.linalg.norm(error_values)
+        assert(total_error < 1E-8)
+
+    @pytest.mark.parametrize('k', np.arange(2,14,2))
+    def test_neighborlists(self,k):
+        """
+        Test that neighborlisting is working correctly.
+        """
+        # Setup reference matrix
+        pw_distance = cdist(x_values, x_values, metric='sqeuclidean')
+        true_values = np.exp(-1.*pw_distance)
+        k0 = min(k,len(x_values))
+        
+        # Construct kernel matrix.
+        mykernel = kernel.Kernel(type='gaussian', metric='euclidean',
+                                 epsilon=1., k=k)
+        mykernel.fit(x_values)
+        K_matrix = mykernel.compute(x_values)
+
+        # Check if each row has correct number of elements
+        row_has_k_elements = (K_matrix.nnz == k0*len(x_values))
+        assert(row_has_k_elements)
