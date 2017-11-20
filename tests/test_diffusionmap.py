9d3dc17619133c99ff07545b8cb91b7ff5d4f21c
Banisch <ralfbanisch@hemingway.imp.fu-berlin.de>
2017-11-20 18:04:53 +0100

    added 2D strip test case

--- tests/test_diffusionmap.py
+++ tests/test_diffusionmap.py
@@ -29,7 +29,7 @@ class TestDiffusionMap(object):
 
         # Check that relative error values are beneath tolerance.
         errors_eval = abs((test_evals - real_evals)/real_evals)
-        total_error = np.min(errors_eval)
+        total_error = np.max(errors_eval)
         assert(total_error < THRESH)
 
     def test_1Dstrip_evecs(self):
@@ -81,7 +81,7 @@ class TestDiffusionMap(object):
 
         # Check that relative error values are beneath tolerance.
         errors_eval = abs((test_evals - real_evals)/real_evals)
-        total_error = np.min(errors_eval)
+        total_error = np.max(errors_eval)
         assert(total_error < THRESH)
 
     def test_1Dstrip_nonunif_evecs(self):
@@ -106,6 +106,60 @@ class TestDiffusionMap(object):
         errors_evec = []
         for k in np.arange(4):
             errors_evec.append(abs(np.corrcoef(np.cos(0.5*(k+1)*X), mydmap.evecs[:, k])[0, 1]))
+
+        # Check that relative error values are beneath tolerance.
+        total_error = 1 - np.min(errors_evec)
+        assert(total_error < THRESH)
+
+    def test_2Dstrip_evals(self):
+        """
+        Test that we compute the correct eigenvalues on a 1d strip of length 2*pi.
+        Diffusion map parameters in this test are hand-selected to give good results.
+        Eigenvalue approximation will fail if k is set too small, or epsilon not optimal (sensitive).
+        """
+        # Setup true values to test again.
+        # real_evals = kx^2 + ky^2 for kx = 0.5*[1 0 2 1] and ky = [0 1 0 1].
+        real_evals = 0.25*np.array([1, 4, 4, 5])
+        # Setup data and accuracy threshold
+        m = 5000
+        X = 2.0*np.pi*np.random.rand(m)
+        Y = 1.0*np.pi*np.random.rand(m)
+        data = np.array([X, Y]).transpose()
+        THRESH = 5*3.0/np.sqrt(m)
+
+        eps = 0.02
+        mydmap = dm.DiffusionMap(n_evecs=4, epsilon=eps, alpha=1.0, k=200)
+        mydmap.fit_transform(data)
+        test_evals = -4./eps*(mydmap.evals - 1)
+
+        # Check that relative error values are beneath tolerance.
+        errors_eval = abs((test_evals - real_evals)/real_evals)
+        total_error = np.max(errors_eval)
+        assert(total_error < THRESH)
+
+    def test_2Dstrip_evecs(self):
+        """
+        Test that we compute the correct eigenvectors (cosines) on a 1d strip of length 2*pi.
+        Diffusion map parameters in this test are hand-selected to give good results.
+        Eigenvector approximation will fail if epsilon is set way too small or too large (robust).
+        """
+        # Setup true values to test again.
+        # real_evecs = cos(kx*x)*cos(ky*y) for kx = 0.5*[1 0 2 1] and ky = [0 1 0 1].
+        # Setup data and accuracy threshold
+        m = 5000
+        X = 2.0*np.pi*np.random.rand(m)
+        Y = 1.0*np.pi*np.random.rand(m)
+        data = np.array([X, Y]).transpose()
+        THRESH = 0.3/np.sqrt(m)
+
+        eps = 0.05
+        mydmap = dm.DiffusionMap(n_evecs=4, epsilon=eps, alpha=1.0, k=200)
+        mydmap.fit_transform(data)
+        errors_evec = []
+        errors_evec.append(abs(np.corrcoef(np.cos(0.5*1*X), mydmap.evecs[:, 0])[0, 1]))
+        errors_evec.append(abs(np.corrcoef(np.cos(Y), mydmap.evecs[:, 1])[0, 1]))
+        errors_evec.append(abs(np.corrcoef(np.cos(0.5*2*X), mydmap.evecs[:, 2])[0, 1]))
+        errors_evec.append(abs(np.corrcoef(np.cos(0.5*1*X)*np.cos(Y), mydmap.evecs[:, 3])[0, 1]))
 
         # Check that relative error values are beneath tolerance.
         total_error = 1 - np.min(errors_evec)
