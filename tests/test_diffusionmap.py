83a5c4817887a0c12aa8efce7bebd6e1e8485cc4
Erik Henning Thiede <ehthiede@gmail.com>
2017-11-26 12:49:08 -0600

    Updated TMDmap test to have nontrivial reweighting

--- tests/test_diffusionmap.py
+++ tests/test_diffusionmap.py
@@ -255,28 +255,26 @@ class TestTMDiffusionMap(object):
     @pytest.mark.parametrize('choose_eps', ['fixed', 'bgh'])
     def test_1Dstrip_evals(self, choose_eps):
         """
-        Test that we compute the correct eigenvalues on a 1d strip of length 2*pi.
-        Using the data set of uniformly distributed points on 1 dimensional periodic domain [0,2 pi],
-        the TMDmap with constat vector equal to one as target density vector
-         (i.e. for flat potential exp(-V(q)) with V = 0).
-        The TMDmap with constant (equal to one) target-measure gives diffusion maps for alpha=1.0.
-        Diffusion map parameters in this test are hand-selected to give good results.
-        Eigenvalue approximation will fail if k is set too small, or epsilon not optimal (sensitive).
+        Test measure reweighting.  We reweight the uniform distribution to
+        approximate a Gaussian distribution.  For numerical reasons, we truncate
+        the domain to the interval [-5, 5].
+
+        Here, we test eigenvalue accuracy.  Eigenvalues should be the counting
+        numbers.
         """
         # Setup true values to test again.
-        # real_evals = k^2 for k in 0.5*[1 2 3 4]
-        real_evals = 0.25*np.array([1, 4, 9, 16])
+        real_evals = np.arange(1, 5)
         # Setup data and accuracy threshold
-        X = np.linspace(0., 1., 81)*2.*np.pi
+        X = np.linspace(-5., 5., 201)
         data = np.array([X]).transpose()
-        THRESH = 0.05
+        THRESH = 0.003
         # Setup diffusion map
         eps = 0.005
 
-        target_distribution = np.ones(len(data))
-        mytmdmap = dm.DiffusionMap(n_evecs=4, epsilon=eps, choose_eps=choose_eps, k=100)
-        mytmdmap.fit_transform(data, weights=target_distribution)
-        test_evals = -4./mytmdmap.epsilon*(mytmdmap.evals - 1)
+        target_distribution = np.exp(-.5*X**2)
+        mydmap = dm.DiffusionMap(n_evecs=4, epsilon=eps, choose_eps=choose_eps, k=100)
+        mydmap.fit_transform(data, weights=target_distribution)
+        test_evals = -4./mydmap.epsilon*(mydmap.evals - 1)
 
         # Check that relative error values are beneath tolerance.
         errors_eval = abs((test_evals - real_evals)/real_evals)
@@ -286,29 +284,30 @@ class TestTMDiffusionMap(object):
     @pytest.mark.parametrize('choose_eps', ['fixed', 'bgh'])
     def test_1Dstrip_evecs(self, choose_eps):
         """
-        Test that we compute the correct eigenvectors (cosines) on a 1d strip of length 2*pi.
-        The TMDmap with constant (equal to one) target-measure gives diffusion maps for alpha=1.0.
-        Diffusion map parameters in this test are hand-selected to give good results.
-        Eigenvector approximation will fail if epsilon is set way too small or too large (robust).
+        Test measure reweighting.  We reweight the uniform distribution to
+        approximate a Gaussian distribution.  For numerical reasons, we truncate
+        the domain to the interval [-5, 5].
+
+        Here, we test eigenvector accuracy.  Eigenvectors should be the
+        probabalists Hermite polynomials.
         """
-        # Setup true values to test again.
-        # real_evecs = cos(k*x) for k in 0.5*[1 2 3 4]
         # Setup data and accuracy threshold
-        X = np.linspace(0., 1., 81)*2.*np.pi
+        X = np.linspace(-5., 5., 201)
         data = np.array([X]).transpose()
-        THRESH = 0.05
+        THRESH = 0.005
+        # Setup true values to test again.
+        real_evecs = [X, X**2-1, X**3-3*X, X**4-6*X**2+3]  # Hermite polynomials
         # Setup diffusion map
         eps = 0.005
-        target_distribution = np.ones(len(data))
-        mytmdmap = dm.DiffusionMap(n_evecs=4, epsilon=eps, choose_eps=choose_eps, k=100)
-        mytmdmap.fit_transform(data, weights=target_distribution)
+        target_distribution = np.exp(-.5*X**2)
+        mydmap = dm.DiffusionMap(n_evecs=4, epsilon=eps, choose_eps=choose_eps, k=100)
+        mydmap.fit_transform(data, weights=target_distribution)
         errors_evec = []
         for k in np.arange(4):
-            errors_evec.append(abs(np.corrcoef(np.cos(0.5*(k+1)*X), mytmdmap.evecs[:, k])[0, 1]))
+            errors_evec.append(abs(np.corrcoef(real_evecs[k], mydmap.evecs[:, k])[0, 1]))
 
         # Check that relative error values are beneath tolerance.
         total_error = 1 - np.min(errors_evec)
-
         assert(total_error < THRESH)
 
 
