1868a7bd08841332e34ba577422c9fca8682b6d5
Banisch <ralfbanisch@hemingway.imp.fu-berlin.de>
2017-11-27 11:18:08 +0100

    created data plot

--- src/pydiffmap/visualization.py
+++ src/pydiffmap/visualization.py
@@ -28,3 +28,24 @@ def embedding_plot(DiffusionMapObject, color=None, size=None):
     plt.ylabel(r'$\psi_2$')
     plt.axis('tight')
     plt.show()
+
+def data_plot(DiffusionMapObject, n_evec=1, size=None):
+    """
+    Creates diffusion map embedding scatterplot. By default, the first two diffusion
+    coordinates are plotted against each other.
+
+    Parameters
+    ----------
+    DiffusionMapObject : An instance of the DiffusionMap class.
+    n_evec: The eigenfunction that should be used to color the plot.
+    size : array-like, shape (n_query,) where n_query is the number of data points passed to
+        DiffusionMap.fit(). Customizes the size of points in the scatter plot.
+
+    """
+    plt.figure(figsize=(6,6))
+    plt.scatter(DiffusionMapObject.data[:,0],DiffusionMapObject.data[:,1], c=DiffusionMapObject.dmap[:,n_evec-1], s=size, cmap=plt.cm.Spectral)
+    plt.title('data coloured with first DC.')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.axis('tight')
+    plt.show()
