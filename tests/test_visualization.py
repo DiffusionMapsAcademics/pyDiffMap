a68bb131239b1463e9274069b504edc63d7ff143
Erik Henning Thiede <ehthiede@gmail.com>
2017-11-27 13:00:09 -0600

    TST: Added tests for visualization

---
+++ tests/test_visualization.py
@@ -0,0 +1,49 @@
+import matplotlib.colors as colors
+import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+
+from pydiffmap import diffusion_map as dm
+from pydiffmap import visualization as viz
+
+
+@pytest.fixture(scope='module')
+def dummy_dmap():
+    data = np.linspace(0., 2.*np.pi, 81).reshape(-1, 1)
+    mydmap = dm.DiffusionMap(n_evecs=2, k=5)
+    mydmap.fit(data)
+    return mydmap
+
+
+class TestEmbeddingPlot():
+    def test_no_kwargs(self, dummy_dmap):
+        mydmap = dummy_dmap
+        fig = viz.embedding_plot(mydmap, scatter_kwargs=None, show=False)
+        assert(fig)
+
+    def test_fixed_coloring(self, dummy_dmap):
+        mydmap = dummy_dmap
+        scatter_kwargs = {'c': 'r'}
+        true_coloring = colors.to_rgba('r')
+        fig = viz.embedding_plot(mydmap, scatter_kwargs, show=False)
+        SC = fig.axes[0].collections[0]
+        print(SC._facecolors[0])
+        assert(np.all(SC._facecolors[0] == true_coloring))
+
+    @pytest.mark.parametrize('size', [4., np.arange(1, 82)])
+    def test_size(self, dummy_dmap, size):
+        mydmap = dummy_dmap
+        scatter_kwargs = {'s': size}
+        fig = viz.embedding_plot(mydmap, scatter_kwargs, show=False)
+        SC = fig.axes[0].collections[0]
+        actual_sizes = SC.get_sizes()
+        assert(np.all(actual_sizes == size))
+
+    @pytest.mark.parametrize('cmap', [None, 'viridis', plt.cm.spectral])
+    def test_colormap(self, dummy_dmap, cmap):
+        # This just tests if the code runs...
+        # Replace with something more stringent?
+        mydmap = dummy_dmap
+        scatter_kwargs = {'c': mydmap.dmap[:, 0], 'cmap': cmap}
+        fig = viz.embedding_plot(mydmap, scatter_kwargs, show=False)
+        assert(fig)
