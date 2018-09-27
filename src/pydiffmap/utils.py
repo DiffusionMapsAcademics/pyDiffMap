e19bb8b8cfbd6f47c29c68c9fa7591763f9e194c
Erik Henning Thiede <ehthiede@gmail.com>
2018-09-27 15:23:35 -0500

    DEV: Added utils for building sparse matrices on pairwise functions

--- src/pydiffmap/utils.py
+++ src/pydiffmap/utils.py
@@ -2,7 +2,8 @@
 """
 Utilities for constructing diffusion maps.
 """
-
+import numpy as np
+import scipy.sparse as sps
 
 def lookup_fxn(x, vals):
     """
@@ -32,3 +33,42 @@ def lookup_fxn(x, vals):
         return lookup[str(xi)]
 
     return lf
+
+
+def sparse_from_fxn(neighbors, function, Y=None):
+    """
+    For a function f, constructs a sparse matrix where each element is
+    f(Y_i, X_j) if Y_i is a k-nearest neighbor to X_j, and zero otherwise.
+
+    Parameters
+    ----------
+    neighbors : scikit-learn NearestNeighbors object
+        Data structure containing the nearest neighbor information.
+        X values are drawn from the data in this object.
+    function : function
+        Function to apply to the pair Y_i, X_j.  Must take only two arguments
+        and return a number.
+    Y : iterable or None
+        Values corresponding to each column of the matrix.  If None, defaults
+        to the data in the neighbors object.
+
+    Returns
+    -------
+    M : scipy sparse csr matrix
+        Matrix with elements f(Y_i, X_j) for nearest neighbors, and zero
+        otherwise.  Here Y_i is the i'th datapoint in Y, and X_j is the
+        j'th datapoint in the NearestNeighbors object.
+    """
+    X = neighbors._fit_X
+    if Y is None:
+        Y = X
+    knn_graph = neighbors.kneighbors_graph(Y, mode='connectivity').tocoo()
+    row = knn_graph.row
+    col = knn_graph.col
+
+    fxn_vals = []
+    for i, j in zip(row, col):
+        fxn_vals.append(function(Y[i], X[j]))
+    fxn_vals = np.array(fxn_vals)
+
+    return sps.csr_matrix((fxn_vals, (row, col)), shape=knn_graph.shape)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,14 +2,15 @@ import numpy as np
 import pytest
 
 from pydiffmap import utils
+from sklearn.neighbors import NearestNeighbors
+
+x_1d = np.arange(10)
+x_2d = np.arange(20).reshape(10, 2)
+y_1d = np.arange(10) + 0.5
+y_2d = np.arange(20).reshape(10, 2) + 0.5
 
 
 class TestLookupFunction(object):
-    x_1d = np.arange(10)
-    x_2d = np.arange(20).reshape(10, 2)
-    y_1d = np.arange(10)**2
-    y_2d = np.arange(20).reshape(10, 2)**2
-
     @pytest.mark.parametrize('x', [x_1d, x_2d])
     @pytest.mark.parametrize('vals', [y_1d, y_2d])
     def test_lookup_fxn(self, x, vals):
@@ -19,3 +20,17 @@ class TestLookupFunction(object):
         lf = utils.lookup_fxn(x, vals)
         shuffle_y = np.array([lf(xi) for xi in x[shuffle_indices]])
         assert((shuffle_y == vals[shuffle_indices]).all())
+
+
+class TestSparseFromFxn(object):
+    @pytest.mark.parametrize('Y', [y_2d, None])
+    def test_sparse_from_fxn(self, Y):
+        nneighbors = NearestNeighbors(10)
+        nneighbors.fit(x_2d)
+        Y2 = Y
+        if Y2 is None:
+            Y2 = x_2d
+        ref_mat = nneighbors.kneighbors_graph(Y2, mode='distance')
+        dist_fxn = lambda Y, X: np.linalg.norm(Y - X)
+        dist_mat = utils.sparse_from_fxn(nneighbors, dist_fxn, Y)
+        assert(np.linalg.norm((dist_mat - ref_mat).data) < 1e-10)
