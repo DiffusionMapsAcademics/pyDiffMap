075ff40a0bad7d88e80216b0f785606fc319c476
Erik Henning Thiede <ehthiede@gmail.com>
2018-10-29 18:29:40 -0500

    Added utils to __init__, from which it had been omitted in error

--- src/pydiffmap/__init__.py
+++ src/pydiffmap/__init__.py
@@ -6,5 +6,6 @@ from __future__ import absolute_import
 from . import diffusion_map
 from . import kernel
 from . import visualization
+from . import utils
 
-__all__ = ['diffusion_map', 'kernel', 'visualization']
+__all__ = ['diffusion_map', 'kernel', 'visualization', 'utils']
